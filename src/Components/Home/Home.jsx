a9f25550ddf2ab39d900f6c4c0f5ee3f3f699b32
vivek <vivekkumarvky13@gmail.com>
2022-08-03 08:50:20 +0530

    home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
+
 import "./Home.css";
 
 import myImg1 from "../../Images/1BelowNavbar/7e8fb3481025c3fc.webp";
 import myImg2 from "../../Images/1BelowNavbar/16d6bf1b4bfd5570.webp";
-
+import newIMg from "../../Images/3productssliding/c2178c3e3990081c.webp";
 import myImg3 from "../../Images/1BelowNavbar/0039bc2a6ee6f11c.webp";
 import myImg4 from "../../Images/1BelowNavbar/49c59f05f883ba88.webp";
 import myImg5 from "../../Images/1BelowNavbar/51cf23e225361de8.webp";
@@ -12,6 +13,7 @@ import myImg7 from "../../Images/1BelowNavbar/1686808e362b885c.webp";
 import myImg8 from "../../Images/1BelowNavbar/a36df627a145c22a.webp";
 import myImg9 from "../../Images/1BelowNavbar/e25f6846955ac543.webp";
 
+import mobileImg from "../../Images/4offers/D51029920_WL_PD_2022_MSO_CCPC_CategoryCard_379X304._SY304_CB631806232_.jpg";
 import bannerImg1 from "../../Images/6offers/D51472544_PD22_WLA_Uber_Page_Tile_1245x450.jpg";
 
 import bannerImg2 from "../../Images/6offers/D51616348_WLD_PD_22_OnePlus_Ovaltine_5G_1242x450.jpg";
@@ -22,7 +24,19 @@ import bannerImg6 from "../../Images/6offers/xcm_banners_a-ku597-8s99e_1242x550_
 
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import Carousel from "react-elastic-carousel"
+
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 4 }
+];
+
+
 export const Home = () => {
+
 
     const banners = [bannerImg1, bannerImg2, bannerImg3, bannerImg4, bannerImg5, bannerImg6]
      const [ind, setIndex] = useState(0);
@@ -111,14 +125,14 @@ export const Home = () => {
           alt=""
           srcset=""
         />
-     
+
         <img
           className="banklogo"
           src="https://zerocreativity0.files.wordpress.com/2016/05/kotak-logo.jpg?w=736"
           alt=""
           srcset=""
         />
-      
+
         <img
           className="banklogo"
           id="banklogo"
@@ -127,21 +141,46 @@ export const Home = () => {
           srcset=""
         />
         <p id="offerpara">
-            10% Instant 
-            <br />
-            Discount*
+          10% Instant
+          <br />
+          Discount*
         </p>
       </div>
       <div id="fourthbox">
-       <div id="topoffers">
-         
-       </div>
-       <div id="productsoffer">
-
-       </div>
-       <div id="adproduct">
-
-       </div>
+        <div id="topoffers">
+          <h1>Top Offers </h1>
+          <button>VIEW ALL</button>
+        </div>
+        <div id="productsoffer">
+          <Carousel id="carousel" breakPoints={breakPoints}>
+            <div className="offerbox">j</div>
+            <div className="offerbox">k</div>
+            <div className="offerbox">l</div>
+            <div className="offerbox">m</div>
+            <div className="offerbox">n</div>
+            <div className="offerbox">o</div>
+            <div className="offerbox">p</div>
+            <div className="offerbox">q</div>
+          </Carousel>
+        </div>
+        <div id="adproduct">
+          <img id="adimg" src={newIMg} alt="" srcset="" />
+        </div>
+      </div>
+      <div id="fifthbox">
+        <div className="fifthbox1">
+          <h3>Get the best offers on mobiles and accessiories</h3>
+          <img src={mobileImg} alt="" srcset="" />
+        </div>
+        <div className="fifthbox1">
+          <h3>Get the best offers on mobiles and accessiories</h3>
+          <img src={mobileImg} alt="" srcset="" />
+        </div>
+        <div className="fifthbox1" id="lastbox">
+          <h3>Get the best offers on mobiles and accessiories</h3>
+          <img src={mobileImg} alt="" srcset="" />
+        </div>
+        <div></div>
       </div>
     </div>
   );
---
+++ src/Components/Home/item.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export default styled.div`
+  display:flex;
+  justify-content: center;
+  align-items:center;
+  heigth: 250px ;
+  width: 100%;
+  background-color: blue;
+  margin: 0 15px
+  font-size: 4em;
+`
